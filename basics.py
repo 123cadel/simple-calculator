3b0ff96ee0d0e4aae874cf1a1a61fe55fe5f7f1e
123cadel <105977053+123cadel@users.noreply.github.com>
2022-05-21 17:03:28 +0400

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,11 @@
+a=20
+v=2
+sum=a+v
+diff=a-v
+mult=a*v
+div=a/v
+
+print("sum of numbers is ",sum,)
+print('difference of numbers is',diff)
+print('multiple of numbers is',mult)
+print('divid of numbers is ',div)
